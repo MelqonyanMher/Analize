9f819379e576071c0821c0c2e0a5778ac52075fa
mic.armenia <mic.armenia@hotmail.com>
2019-05-22 01:51:49 +0400

    Add DbModels

---
+++ Analizer.NetCore/Models/FireRiskContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Analizer.NetCore.Models
+{
+    public class FireRiskContext:DbContext
+    {
+        public FireRiskContext(DbContextOptions<FireRiskContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<FireRiskItam> Itams { get; set; }
+        public DbSet<City> Cities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<FireRiskItam>(e =>
+            {
+                e.HasKey(p => p.Id);
+                e.Property(p => p.Precipitation).HasDefaultValue(0);
+                e.Property(p => p.ClassOfFireRisk).IsRequired();
+                e.Property(p => p.CompIndicator).IsRequired();
+                e.Property(p => p.CompIndicatorDay).IsRequired();
+                e.Property(p => p.Day).IsRequired();
+                e.Property(p => p.Temperature).IsRequired();
+                e.Property(p => p.DewPoint).IsRequired();
+
+
+            });
+            modelBuilder.Entity<FireRiskItam>()
+                .HasOne(p => p.City)
+                .WithMany(p => p.FireRiskItams)
+                .HasForeignKey(p => p.CityId).IsRequired();
+            
+        }
+    }
+}
---
+++ Analizer.NetCore/Models/FireRiskItam.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Analizer.NetCore.Models
+{
+    public class FireRiskItam
+    {
+        public Guid Id { get; set; }
+
+        public Guid CityId { get; set; }
+
+        public DateTime Day { get; set; }
+
+        public double Temperature { get; set; }
+
+        public double DewPoint { get; set; }
+
+        public double CompIndicatorDay { get; set; }
+
+        public int Precipitation { get; set; }
+
+        public double CompIndicator { get; set; }
+
+        public byte ClassOfFireRisk { get; set; }
+
+
+
+        public City City { get; set; }
+
+    }
+}
