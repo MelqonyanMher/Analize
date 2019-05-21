85d8f0a981a6835c0f8373351eb97f8d61b0e673
MherMelkonyan <Melqonyan.mher@hotmail.com>
2019-05-22 02:31:24 +0400

    Add function Db.Migrate()

--- Analizer.NetCore/Startup.cs
+++ Analizer.NetCore/Startup.cs
@@ -41,8 +41,9 @@ namespace Analizer.NetCore
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,FireRiskContext context)
         {
+            context.Database.Migrate();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
