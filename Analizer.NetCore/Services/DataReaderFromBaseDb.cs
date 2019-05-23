fd1e8e0f367730d0f2d5e16049a99bb4d79f0ef4
MherMelkonyan <melqonyan.mher@hotmail.com>
2019-05-23 16:45:30 +0400

    Add DataRiderMeneger

---
+++ Analizer.NetCore/Services/DataReaderFromBaseDb.cs
@@ -0,0 +1,48 @@
+﻿using Analizer.NetCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Analizer.NetCore.Services
+{
+    public class DataReaderFromBaseDb : IDataReaderFromBaseDb
+    {
+        private HidrometDbContext _context;
+        
+        public DataReaderFromBaseDb(HidrometDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<DataItam> GetElemsFromDb(DateTime? startDay)
+        {
+            List<DataItam> data = new List<DataItam>();
+            List<Hidromet> dt;
+            if (startDay != null)
+            {
+                 dt = _context.Hidromet.Where(itm => itm.Date.Value.Day > startDay.Value.Day).ToList();
+            }
+            else
+            {
+                dt = _context.Hidromet.Where(itm => itm.Date.Value.Year==DateTime.Now.Year ).Where(itm=>itm.Date.Value.Month>5&&itm.Date.Value.Month<9).ToList();
+            }
+            foreach(var itam in dt)
+            {
+                data.Add(new DataItam
+                {
+                    City = itam.City,
+                    Temperature = itam.Temperature,
+                    Day = itam.Date.Value,
+                    DewPoint = itam.DewPoint,
+                    Precipitation = itam.Precipitation
+                });
+            }
+            return data;
+        }
+
+        public IEnumerable<DataItam> GetElemsFromDbByYear(int year)
+        {
+            
+        }
+    }
+}
---
+++ Analizer.NetCore/Services/IDataReaderFromBaseDb.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Analizer.NetCore.Services
+{
+    public interface IDataReaderFromBaseDb
+    {
+        IEnumerable<DataItam> GetElemsFromDb(DateTime? startDay);
+        IEnumerable<DataItam> GetElemsFromDbByYear(int year);
+
+    }
+}
