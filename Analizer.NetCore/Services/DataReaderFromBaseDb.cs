183fc8bc7cad3f406933ce698b28a5c045e95893
MherMelkonyan <melqonyan.mher@hotmail.com>
2019-05-24 19:09:44 +0400

    Add function$ in datareader

--- Analizer.NetCore/Services/DataReaderFromBaseDb.cs
+++ Analizer.NetCore/Services/DataReaderFromBaseDb.cs
@@ -9,7 +9,7 @@ namespace Analizer.NetCore.Services
     public class DataReaderFromBaseDb : IDataReaderFromBaseDb
     {
         private HidrometDbContext _context;
-        
+
         public DataReaderFromBaseDb(HidrometDbContext context)
         {
             _context = context;
@@ -17,32 +17,62 @@ namespace Analizer.NetCore.Services
         public IEnumerable<DataItam> GetElemsFromDb(DateTime? startDay)
         {
             List<DataItam> data = new List<DataItam>();
-            List<Hidromet> dt;
+            IQueryable<IGrouping<int,Hidromet>> dt;
             if (startDay != null)
             {
-                 dt = _context.Hidromet.Where(itm => itm.Date.Value.Day > startDay.Value.Day).ToList();
+                 dt = _context.Hidromet.Where
+                    (itm =>itm.Date.Value.Year > startDay.Value.Year
+                    && itm.Date.Value.DayOfYear > startDay.Value.DayOfYear
+                    &&  itm.Date.Value.Month <9)
+                    .GroupBy(itm=>itm.Date.Value.DayOfYear);
             }
             else
             {
-                dt = _context.Hidromet.Where(itm => itm.Date.Value.Year==DateTime.Now.Year ).Where(itm=>itm.Date.Value.Month>5&&itm.Date.Value.Month<9).ToList();
+                dt = _context.Hidromet.Where
+                    (itm => itm.Date.Value.Year == DateTime.Now.Year
+                    && itm.Date.Value.Month > 5
+                    && itm.Date.Value.Month < 9)
+                    .GroupBy(itm=>itm.Date.Value.DayOfYear);
             }
-            foreach(var itam in dt)
+            foreach (var dayGrupItam in dt)
             {
-                data.Add(new DataItam
+                var dtGroupedCity = dayGrupItam.GroupBy(itm => itm.City);
+                foreach (var itam in dtGroupedCity)
                 {
-                    City = itam.City,
-                    Temperature = itam.Temperature,
-                    Day = itam.Date.Value,
-                    DewPoint = itam.DewPoint,
-                    Precipitation = itam.Precipitation
-                });
+                    data.Add(new DataItam
+                    {
+                        City = itam.First().City,
+                        Temperature = itam.Single(itm => itm.Date.Value.Hour == 12).Temperature,
+                        Day = itam.Single(itm => itm.Date.Value.Hour == 12).Date.Value,
+                        DewPoint = itam.Single(itm => itm.Date.Value.Hour == 12).DewPoint,
+                        Precipitation = itam.Sum(itm => itm.Precipitation)
+                    });
+                }
             }
             return data;
         }
 
         public IEnumerable<DataItam> GetElemsFromDbByYear(int year)
         {
-            
+            List<DataItam> data = new List<DataItam>();
+            var dt = _context.Hidromet.Where(itm => itm.Date.Value.Year == year && itm.Date.Value.Month > 5 && itm.Date.Value.Month < 9).GroupBy(itm => itm.Date.Value.DayOfYear);
+
+            foreach (var dayGrupItam in dt)
+            {
+                var dtGroupedCity = dayGrupItam.GroupBy(itm => itm.City);
+                foreach (var itam in dtGroupedCity)
+                {
+                    data.Add(new DataItam
+                    {
+                        City = itam.First().City,
+                        Temperature = itam.Single(itm => itm.Date.Value.Hour == 12).Temperature,
+                        Day = itam.Single(itm => itm.Date.Value.Hour == 12).Date.Value,
+                        DewPoint = itam.Single(itm => itm.Date.Value.Hour == 12).DewPoint,
+                        Precipitation = itam.Sum(itm => itm.Precipitation)
+                    });
+                }
+            }
+            return data;
         }
     }
 }
