23ecdb42dd736c6de94fd61728de796b46a99733
MherMelkonyan <melqonyan.mher@hotmail.com>
2019-05-30 20:41:57 +0400

    Add graph model

---
+++ Analizer.NetCore/Models/GraphModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace Analizer.NetCore.Models
+{
+    [DataContract]
+    public class GraphModel
+    {
+        public GraphModel(string d,int lev)
+        {
+            Day = d;
+            Level = lev;
+        }
+        [DataMember(Name ="day")]
+        public string Day { get; set; }
+
+        [DataMember(Name ="level")]
+        public int Level { get; set; }
+    }
+}
